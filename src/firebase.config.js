0740f947ebfce6a37e231b079a24a22c4a3d4913
panosjapan7 <panosjapan@gmail.com>
2022-06-28 13:31:59 +0200

    Setup project

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbc-TflxMiO1TKjM1NHqaflTKW0bgMKhE",
+  authDomain: "house-marketplace-react-7a1c9.firebaseapp.com",
+  projectId: "house-marketplace-react-7a1c9",
+  storageBucket: "house-marketplace-react-7a1c9.appspot.com",
+  messagingSenderId: "537471262444",
+  appId: "1:537471262444:web:8a3c5881eda24c86c80fc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
