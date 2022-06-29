6e76f3186c836431ab76a7e45f8d88ca02d12bec
panosjapan7 <panosjapan@gmail.com>
2022-06-29 08:56:27 +0200

    User Sign In

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+
 function Profile() {
+  const [user, setUser] = useState(null);
+  
+  const auth = getAuth();
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+
   return (
-    <div>
-        <h1>Profile</h1>
-    </div>
-  )
+    user ? <h1>{user.displayName}</h1> : "Not logged in"
+    )
 }
 
 export default Profile
